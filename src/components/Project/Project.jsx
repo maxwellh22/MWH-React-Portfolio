e195584c3c49929b612d5666def4411b70543386
Maxwell Hurst <maxhurst120898@gmail.com>
2025-02-09 14:52:44 -0500

    updated Project.jsx to fix image preview issue on Portfolio page

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./Project.css";
 
-const Project = ({ title, link }) => {
-    return (
-      <div className="project">
-        <h3>{title}</h3>
-        <a href={link} target="_blank" rel="noreferrer">
-          Visit Project
-        </a>
-      </div>
-    );
-  };
+const Project = ({ title, link, image }) => {
+  return (
+    <div className="project">
+      <img src={image} alt={title} className="project-image" />
+      <h3>{title}</h3>
+      <a href={link} target="_blank" rel="noreferrer">
+        Visit Project
+      </a>
+    </div>
+  );
+};
 
 export default Project;
